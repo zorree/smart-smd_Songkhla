eb1ecd6f009da1432dd166b5ab5c64c4192b400d
zorree <zorree.nat@gmail.com>
2022-11-16 00:27:08 +0700

    add news to server

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,13 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { urls, ports, stations, url, port, station } from '../../assets/url/url';
+import { host, port, station } from '../../assets/url/url';
 
 import * as fs from 'fs';
 
+import { io } from "../../../node_modules/socket.io-client";
+
 // import dataUrl from '../../assets/data/data.json';
 
-let urla = 'http://' + url.url + ':' + port.port + '/api/fn_push_que/'
+let urlServer: string = 'http://' + host + ':' + port
+
+// let socket = io(urlServer)
+
+const socket = io(urlServer, {});
 
 @Component({
   selector: 'app-home',
@@ -17,26 +23,23 @@ let urla = 'http://' + url.url + ':' + port.port + '/api/fn_push_que/'
 })
 export class HomeComponent implements OnInit {
 
-  left = ''
-  right = ''
-  c = ''
-  postId = ''
-  chanel = 1
+  hostTemplate: any = host
+  portTemplate: any = port
+  stationTemplate: any = station
 
-  url1 = localStorage.getItem('url')
-  port1 = localStorage.getItem('port')
-  station = localStorage.getItem('station')
+  left: any
+  right: any
 
-  range = localStorage.getItem('station')
+  news: string = 'ยินดีต้อนรับ'
+  newsEdit: string = ''
 
-  url1e: any
-  port1e: any
-  statione: any
+  displayBootstrap = ['d-none', '']
 
-  url5 = url.url
-  port5 = port.port
+  displayCheck: any
 
-  total: any
+  hostEdit: any
+  portEdit: any
+  stationEdit: any
 
   constructor(
     private http: HttpClient
@@ -44,97 +47,130 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.url1e = this.url1
-    this.port1e = this.port1
-    this.statione = this.station
-    if (this.url1) {
-      console.log('OK URL')
-      console.log(this.url1)
+    if (this.hostTemplate) {
+      console.log('This host is ' + this.hostTemplate);
     } else {
-      localStorage.setItem('url', "192.168.4.245");
-      this.url1 = localStorage.getItem('url')
-      this.url1e = localStorage.getItem('url')
+      localStorage.setItem('host', "192.168.4.245");
+      this.hostTemplate = localStorage.getItem('host');
+      console.log('This host was chenge' + this.hostTemplate);
     }
 
-    if (this.port1) {
-      console.log('OK PORT')
-      console.log(this.port1)
+    if (this.portTemplate) {
+      console.log('This port is ' + this.portTemplate);
     } else {
       localStorage.setItem('port', "3040");
-      this.port1 = localStorage.getItem('port')
-      this.port1e = localStorage.getItem('port')
+      console.log('This port was chenge' + this.portTemplate);
     }
 
-    if (this.station) {
-      console.log('OK station')
-      console.log(this.station)
+    if (this.stationTemplate) {
+      console.log('This station is ' + this.stationTemplate);
     } else {
       localStorage.setItem('station', "2");
-      this.station = localStorage.getItem('station')
-      this.statione = localStorage.getItem('station')
+      this.stationTemplate = localStorage.getItem('station');
     }
 
-    urla = 'http://' + this.url1e + ':' + this.port1e + '/api/fn_push_que/'
-    this.total = urla
-    console.log(urla)
+    urlServer = 'http://' + this.hostTemplate + ':' + this.portTemplate;
+    console.log('Server URL is ' + urlServer);
+
+    this.hostEdit = this.hostTemplate
+    this.portEdit = this.portTemplate
+    this.stationEdit = this.stationTemplate
+
+    this.http.get(urlServer + '/api/getnews').subscribe((data:any) => {
+      // console.log(data.news);
+      this.news = data.news
+    })
+
   }
 
-  check() {
+  pushQue() {
 
     if (this.left.length != 0 && this.right.length != 0) {
       if (this.left < this.right) {
-        this.c = this.left + '-' + this.right
-        console.log(this.c);
-        this.left = ''
-        this.right = ''
-        this.http.post<any>(urla, {
-          station_id: this.station,
-          number: this.c
+
+        console.log('sent QUE ' + this.left + '-' + this.right);
+
+        this.http.post(urlServer + '/api/fn_push_que', {
+          station_id: this.stationTemplate,
+          number: this.left + '-' + this.right
         }).subscribe(data => {
-          this.postId = data;
-          console.log(data)
+          console.log(data);
         })
+
+        this.left = '';
+        this.right = '';
+
       } else {
-        console.log('NO')
+        console.log('cannot sent QUE');
       }
     }
 
   }
 
-  checks() {
-    console.log(this.range)
-    if (this.range) {
-      localStorage.setItem('station', this.range);
-      this.station = this.range
-    }
+  clearQue() {
+
+    console.log('clearQue')
+
+    this.http.post(urlServer + '/api/clearQue', {
+      station_id: this.stationTemplate
+    }).subscribe(data => {
+      console.log(data);
+    })
+
   }
 
-  onSubmit() {
-    // Process checkout data here
-    // urla = 'http://' + this.url5 + ':' + this.port5 + '/api/fn_push_que/'
-    // this.total = urla
-    // stations(this.station)
-    // urls(this.url5)
-    // ports(this.port5)
-    // console.log(url)
-    // console.log(port)
-    // console.log(station)
+  selectSetting() {
 
-    localStorage.setItem('url', this.url1e);
-    localStorage.setItem('port', this.port1e);
-    localStorage.setItem('station', this.statione);
+    console.log('selectSetting')
 
-    this.url1 = localStorage.getItem('url')
-    this.port1 = localStorage.getItem('port')
-    this.station = localStorage.getItem('station')
-
-    this.range = this.station
-
-    this.total = 'http://' + localStorage.getItem('url') + ':' + localStorage.getItem('port') + '/api/fn_push_que/'
-    urla = this.total
-    console.log(this.total)
   }
 
+  selectNews() {
 
+    console.log('selectNews')
+    this.newsEdit = this.news
+
+  }
+
+  checkStation() {
+
+    console.log('checkStation')
+    localStorage.setItem('station', this.stationTemplate);
+    this.stationEdit = this.stationTemplate
+
+  }
+
+  onSaveNews() {
+
+    console.log('onSaveNews')
+    this.news = this.newsEdit
+
+    this.http.post(urlServer + '/api/saveNews', {
+      news: this.news
+    }).subscribe(data => {
+      console.log(data);
+    })
+
+  }
+
+  onSaveSettting() {
+
+    console.log('onSaveSettting')
+
+    localStorage.setItem('host', this.hostEdit);
+    localStorage.setItem('port', this.portEdit);
+    localStorage.setItem('station', this.stationEdit);
+
+    this.hostTemplate = this.hostEdit
+    this.portTemplate = this.portEdit
+    this.stationTemplate = this.stationEdit
+
+    urlServer = 'http://' + this.hostEdit + ':' + this.portEdit;
+    console.log(urlServer);
+
+    // socket = io(urlServer)
+    console.log('Socket ip Was Chenges');
+
+  }
 
 }
--- src/assets/url/url.ts
+++ src/assets/url/url.ts
@@ -1,26 +1,5 @@
-import dataUrl from '../../assets/data/data.json';
+var host = localStorage.getItem('host');
+var port = localStorage.getItem('port');
+var station = localStorage.getItem('station');
 
-// var url = { url: dataUrl.url };
-// var port = { port: dataUrl.port };
-// var station = { station: dataUrl.staton };
-
-var url = { url: localStorage.getItem('url') };
-var port = { port: localStorage.getItem('port') };
-var station = { station: localStorage.getItem('station') };
-
-function urls(user: any) {
-    url.url = user
-    // return url
-}
-
-function ports(user: any) {
-    port.port = user
-    // return port
-}
-
-function stations(user: any) {
-    station.station = user
-    // return station
-}
-
-export { urls, ports, stations, url, port, station };
+export { host, port, station };
